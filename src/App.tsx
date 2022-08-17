64cfcba345ef421e37262dae7b922e92c3b1bd05
SeriousAndres <soyandresbernal@gmail.com>
2022-08-17 15:13:27 -0500

    start dapp

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  const FETCH_POKEMON_BY_NAME = "FETCH_POKEMON_BY_NAME";
+  const fetchPokemonByName = () => ({
+    type: FETCH_POKEMON_BY_NAME,
+    name,
+  });
+  const pokemonsReducer = (state = {}, action) => {
+    switch (action.type) {
+      case FETCH_POKEMON_BY_NAME:
+        return {
+          ...state,
+          pokemon: action.name,
+        };
+      default:
+        return state;
+    }
+  };
+  return (
+    <>
+      <h1>Start!</h1>
+    </>
+  );
+}
+export default App;
