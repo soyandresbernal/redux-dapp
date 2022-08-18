2f9bc5fa810c295faaaf90476e42ad3a034fcc2e
SeriousAndres <soyandresbernal@gmail.com>
2022-08-18 12:58:18 -0500

    feat reducers

---
+++ src/redux/reducers/index.jsx
@@ -0,0 +1,15 @@
+import { combineReducers } from "@reduxjs/toolkit";
+
+// Reducers
+
+import results from "./results";
+import detail from "./detail";
+
+// Slices
+import detailSlice from "../slices/detail";
+
+export default combineReducers({
+  results,
+  detail,
+  detailSlice,
+});
---
+++ src/redux/reducers/results.jsx
@@ -0,0 +1,30 @@
+import {
+  FETCH_RECIPES_COMPLETE,
+  FETCH_RECIPES_ERROR,
+  FETCH_RECIPES_START,
+  ADD_SEARCH_ITEM,
+} from "../actions/results";
+
+const initialState = {
+  isLoading: false,
+  data: [],
+  error: {},
+  searchItems: [],
+};
+
+const resultsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_RECIPES_START:
+      return { ...state, isLoading: true, data: [] };
+    case FETCH_RECIPES_COMPLETE:
+      return { ...state, isLoading: false, data: action.payload };
+    case FETCH_RECIPES_ERROR:
+      return { ...state, isLoading: false, error: action.error };
+    case ADD_SEARCH_ITEM:
+      return { ...state, searchItems: [...state.searchItems, action.payload] };
+    default:
+      return state;
+  }
+};
+
+export default resultsReducer;
